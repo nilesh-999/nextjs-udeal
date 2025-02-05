92f94af3bc7d826c70d3ca08f81fe46e20412790
NTG <nilesh.23a@gmail.com>
2025-02-05 15:20:14 +0530

    carousel update

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,7 @@
+import HomeCarousel from "@/components/shared/home/home-carousel";
+import data from '@/lib/data'
 export default async function Page() {
   return (
-    <div>
-      <h1 className='h1-bold text-center p-3'>Launching Soon</h1>
-      <h2 className='h2 text-center p-3'>Under Development</h2>
-    </div>
+    <HomeCarousel items={data.carousels} />
   )
 }

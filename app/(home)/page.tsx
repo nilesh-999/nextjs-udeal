7f834b39882213cdfe853cce9ee34bef489590fe
NTG <nilesh.23a@gmail.com>
2025-02-10 23:50:12 +0530

    updated

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,60 @@
-import HomeCarousel from "@/components/shared/home/home-carousel";
+import { HomeCard } from "@/components/shader/home/home-card";
+import  HomeCarousel  from "@/components/shared/home/home-carousel";
+import { getAllCategories, getProductsForCard } from "@/lib/actions/product.actions";
 import data from '@/lib/data'
+import { toSlug } from "@/lib/utils";
+
+
 export default async function Page() {
+  const categories = (await getAllCategories()).slice(0,4)
+  const newArrivals = await getProductsForCard({tag: 'new-arrival', limit: 4})
+  const fetureds = await getProductsForCard({tag: 'featured', limit: 4})
+  const bestSellers = await getProductsForCard({tag: 'best-seller', limit: 4})
+  const cards = [
+    {
+      title: 'Categories to explore',
+      link: {
+        text: 'See More',
+        href: '/search',
+      },
+      items: categories.map((category) => ({
+        name: category,
+        image: `/images/${toSlug(category)}.jpg`,
+        href: `/search?category=${category}`,
+      })),
+    },
+    {
+      title: 'Explore New Arrivals',
+      items: newArrivals,
+      link: {
+        text: 'View All',
+        href: '/search?tag=new-arrival'
+      },
+    },
+    {
+      title: 'Discover Best Sellers',
+      items: bestSellers,
+      link: {
+        text: 'View All',
+        href: '/search?tag=new-arrival',
+      },
+
+    },
+    {
+      title: 'Featured Products',
+      items: fetureds,
+      link: {
+        text: 'Shop Now',
+        href: '/search?tag=new-arrival',
+      },
+    },
+  ]
   return (
-    <HomeCarousel items={data.carousels} />
+    <>
+    <HomeCarousel items={data.carousels}/>
+    <div className='md:p-4 md:space-y-4 bg-border'>
+      <HomeCard cards={cards} />
+    </div>
+    </>
   )
 }

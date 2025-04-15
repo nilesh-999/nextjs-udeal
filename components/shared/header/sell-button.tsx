f34ff9494fe313b2c9b35553cebeee9221aa9521
NTG <nilesh.23a@gmail.com>
2025-04-15 23:59:36 +0530

    sell-form-update

--- components/shared/header/sell-button.tsx
+++ components/shared/header/sell-button.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 export default function SellButton() {
 
     return(
-        <Link href='https://docs.google.com/forms/d/e/1FAIpQLSdwh6kxt0nYUmvDy9n662SuD1kHEnIYKrRQR3gceALg4r3f6A/viewform' className='px-1 header-button'>
+        <Link href='https://forms.gle/vLRKLSAeeXQaFLcP6' className='px-1 header-button'>
            <Button className='hidden md:block'>
                                        Sell
             </Button>

aba1c7987afcbcfc9868a6b49406bcca95a3dc9e
NTG <nilesh.23a@gmail.com>
2025-08-03 15:59:54 +0530

    minor-change-23

--- emails/index.tsx
+++ emails/index.tsx
@@ -3,41 +3,36 @@ import PurchaseReceiptEmail from './purchase-receipt'
 import { IOrder } from '@/lib/db/model.order.model'
 import { SENDER_EMAIL, SENDER_NAME } from '@/lib/constants'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+if (!process.env.RESEND_API_KEY) {
+  throw new Error('RESEND_API_KEY is not defined in environment variables');
+}
 
-// export const sendPurchaseReceipt = async ({ order }: { order: IOrder }) => {
-//   try {
-//     const userEmail = typeof order.user === 'string'
-//       ? order.user
-//       : order.user.email;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-//     console.log('Preparing to send email:', {
-//       from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
-//       to: userEmail,
-//       subject: 'Order Confirmation',
-//       order: order._id
-//     });
-
-//     const response = await resend.emails.send({
-//       from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
-//       to: userEmail,
-//       subject: 'Order Confirmation',
-//       react: <PurchaseReceiptEmail order={order} />,
-//     });
-
-//     console.log('Email sent successfully:', response);
-//     return response;
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//     throw error;
-//   }
-// }
 export const sendPurchaseReceipt = async ({ order }: { order: IOrder }) => {
-  await resend.emails.send({
-    from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
-    to: (order.user as { email: string }).email,
-    subject: 'Order Confirmation',
-    react: <PurchaseReceiptEmail order={order} />,
-  })
+  try {
+    const userEmail = typeof order.user === 'string'
+      ? order.user
+      : order.user.email;
+
+    console.log('Attempting to send email with details:', {
+      from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
+      to: userEmail,
+      subject: 'Order Confirmation'
+    });
+
+    const response = await resend.emails.send({
+      from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
+      to: userEmail,
+      subject: 'Order Confirmation',
+      react: <PurchaseReceiptEmail order={order} />,
+    });
+
+    console.log('Email sent successfully:', response);
+    return response;
+  } catch (error) {
+    console.error('Failed to send email:', error);
+    throw error;
+  }
 }
 

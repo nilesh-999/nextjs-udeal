05cdcb94be6d505b9e20f0441ca2d8e8dd409e2c
NTG <nilesh.23a@gmail.com>
2025-08-03 19:19:35 +0530

    minor-change-29

--- emails/index.tsx
+++ emails/index.tsx
@@ -3,9 +3,9 @@ import PurchaseReceiptEmail from './purchase-receipt'
 import { IOrder } from '@/lib/db/model.order.model'
 import { SENDER_EMAIL, SENDER_NAME } from '@/lib/constants'
 
-//if (!process.env.RESEND_API_KEY) {
-  //throw new Error('RESEND_API_KEY is not defined in environment variables');
-//}
+if (!process.env.RESEND_API_KEY) {
+  throw new Error('RESEND_API_KEY is not defined in environment variables');
+}
 console.log('Using Resend API Key:', process.env.RESEND_API_KEY);
 
 const resend = new Resend(process.env.RESEND_API_KEY);

f94c80c5ed70226b0fef0f8fc5701601e74426d5
NTG <nilesh.23a@gmail.com>
2025-08-03 20:37:38 +0530

    minor-change-39

--- emails/index.tsx
+++ emails/index.tsx
@@ -2,7 +2,7 @@ import { Resend } from 'resend'
 import PurchaseReceiptEmail from './purchase-receipt'
 import { IOrder } from '@/lib/db/models/order.model'
 import { SENDER_EMAIL, SENDER_NAME } from '@/lib/constants'
-import User from '@/lib/db/models/user.model';
+//import User from '@/lib/db/models/user.model';
 
 
 if (!process.env.NEXT_PUBLIC_RESEND_API_KEY) {
@@ -13,10 +13,10 @@ console.log('Using Resend API Key:', process.env.NEXT_PUBLIC_RESEND_API_KEY);
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY as string);
 
 export const sendPurchaseReceipt = async ({ order }: { order: IOrder }) => {
-  console.log('Sending purchase receipt for order:', (User.findById(order.user)));
+  //console.log('Sending purchase receipt for order:', (User.findById(order.user)));
   await resend.emails.send({
     from: `${SENDER_NAME} <${SENDER_EMAIL}>`,
-    to: (order.user as { email: string }).email,
+    to: 'nilesh.23a@gmail.com',
     subject: 'Order Confirmation',
     react: <PurchaseReceiptEmail order={order} />,
   })

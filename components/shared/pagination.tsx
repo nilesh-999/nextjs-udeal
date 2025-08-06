880902339fab9f2417d47ad0ffe6e01c2f48e8f3
NTG <nilesh.23a@gmail.com>
2025-08-06 20:34:40 +0530

    order-history

---
+++ components/shared/pagination.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useRouter, useSearchParams } from 'next/navigation'
+import React from 'react'
+
+import { formUrlQuery } from '@/lib/utils'
+
+import { Button } from '../ui/button'
+
+type PaginationProps = {
+  page: number | string
+  totalPages: number
+  urlParamName?: string
+}
+
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const onClick = (btnType: string) => {
+    const pageValue = btnType === 'next' ? Number(page) + 1 : Number(page) - 1
+
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: urlParamName || 'page',
+      value: pageValue.toString(),
+    })
+
+    router.push(newUrl, { scroll: true })
+  }
+
+  return (
+    <div className='flex items-center gap-2'>
+      <Button
+        size='lg'
+        variant='outline'
+        onClick={() => onClick('prev')}
+        disabled={Number(page) <= 1}
+        className='w-28'
+      >
+        Previous
+      </Button>
+      <Button
+        size='lg'
+        variant='outline'
+        onClick={() => onClick('next')}
+        disabled={Number(page) >= totalPages}
+        className='w-24'
+      >
+        Next
+      </Button>
+    </div>
+  )
+}
+
+export default Pagination

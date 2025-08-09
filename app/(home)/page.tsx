5938e9724a49fc8e5410d1a812cfe674866806be
NTG <nilesh.23a@gmail.com>
2025-08-09 12:58:43 +0530

    minor-change

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -22,7 +22,7 @@ export default async function Page() {
       },
       items: categories.map((category) => ({
         name: category,
-        image: `/images/${toSlug(category)}.jpg`,
+        image: `/images/Academic-tools.jpg`,
         href: `/search?category=${category}`,
       })),
     },
